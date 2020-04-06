35c7f33a80d5280a58f7b6b1438c6ce47abda004
ryan3738 <ryan3738@gmail.com>
2020-04-06 16:06:48 -0700

    Searching for Videos
    Setting up the youtub api

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from "axios";
+
 const KEY = "AIzaSyBCHusKVj2eeJWklEMtrlX8prtKk5eqseQ";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY
+  }
+});
