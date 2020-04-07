8737bcfe19f05688f4844e11b9ed8d3fb065ff3d
ryan3738 <ryan3738@gmail.com>
2020-04-06 17:48:02 -0700

    Deeply nested callbacks

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import VideoList from "./VideoList";
 
 const KEY = "AIzaSyBCHusKVj2eeJWklEMtrlX8prtKk5eqseQ";
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
 
   onTermSubmit = async term => {
     const response = await youtube.get("/search", {
@@ -21,6 +21,10 @@ class App extends React.Component {
     this.setState({ videos: response.data.items });
   };
 
+  onVideoSelect = video => {
+    console.log("From the App!", video);
+  };
+
   render() {
     return (
       <div className="ui container">
@@ -28,7 +32,10 @@ class App extends React.Component {
           Typical convention is to match it to the callback function name.
           In this case that is "onTermSubmit" */}
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <VideoList
+          onVideoSelect={this.onVideoSelect}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
