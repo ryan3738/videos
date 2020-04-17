8652c1e21db468fdfcc02219cd0ac6f322adfee5
ryan3738 <ryan3738@gmail.com>
2020-04-17 14:37:05 -0700

    Updates

---
+++ precache-manifest.7dfae92cd3656f31f4a715f55e189965.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4138661656e1ac4671d280f7b691197e",
+    "url": "/videos/index.html"
+  },
+  {
+    "revision": "456d9d84a36123ddee1a",
+    "url": "/videos/static/css/main.5157b21c.chunk.css"
+  },
+  {
+    "revision": "ee529c831dd079b6fcb0",
+    "url": "/videos/static/js/2.4c8536a3.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/videos/static/js/2.4c8536a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "456d9d84a36123ddee1a",
+    "url": "/videos/static/js/main.74f701f7.chunk.js"
+  },
+  {
+    "revision": "373cf2b69139c84bfd28",
+    "url": "/videos/static/js/runtime-main.ba7d5d3e.js"
+  }
+]);
